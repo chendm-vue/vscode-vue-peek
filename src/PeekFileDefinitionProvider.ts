bf7b266b64262d0e8f7da843d11494ade2aa103e
chendm <2283973011@qq.com>
2020-10-22 16:36:26 +0800

    add Config

--- src/PeekFileDefinitionProvider.ts
+++ src/PeekFileDefinitionProvider.ts
@@ -1,35 +1,40 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 export default class PeekFileDefinitionProvider implements vscode.DefinitionProvider {
-  targetFileExtensions: string[] = [];
+  targetFileExtensions: string[] = []
 
   constructor(targetFileExtensions: string[] = []) {
-    this.targetFileExtensions = targetFileExtensions;
+    this.targetFileExtensions = targetFileExtensions
   }
 
   getComponentName(position: vscode.Position): String[] {
-    const doc = vscode.window.activeTextEditor.document;
-    const selection = doc.getWordRangeAtPosition(position);
-    const selectedText = doc.getText(selection);
+    const doc = vscode.window.activeTextEditor.document
+    const selection = doc.getWordRangeAtPosition(position)
+    const selectedText = doc.getText(selection)
     let possibleFileNames = [],
       altName = ''
 
     selectedText.match(/\w+/g).forEach(str => {
-      return altName += str[0].toUpperCase() + str.substring(1);
+      return (altName += str[0].toUpperCase() + str.substring(1))
     })
 
     this.targetFileExtensions.forEach(ext => {
+      const configParams = vscode.workspace.getConfiguration('vue-peek')
+      const selectedTextSplit = configParams.get('selectedTextSplitOnlyCapitalize') as Boolean
+      if (!selectedTextSplit) {
+        possibleFileNames.push(altName[0].toLowerCase() + altName.substring(1) + ext)
+      }
       possibleFileNames.push(selectedText + ext)
       possibleFileNames.push(selectedText + '/index' + ext)
       possibleFileNames.push(altName + ext)
       possibleFileNames.push(altName + '/index' + ext)
     })
 
-    return possibleFileNames;
+    return possibleFileNames
   }
 
   searchFilePath(fileName: String): Thenable<vscode.Uri[]> {
-    return vscode.workspace.findFiles(`**/${fileName}`, '**/node_modules'); // Returns promise
+    return vscode.workspace.findFiles(`**/${fileName}`, '**/node_modules') // Returns promise
   }
 
   provideDefinition(
@@ -37,26 +42,30 @@ export default class PeekFileDefinitionProvider implements vscode.DefinitionProv
     position: vscode.Position,
     token: vscode.CancellationToken
   ): Promise<vscode.Location | vscode.Location[]> {
-    
-    let filePaths = [];
-    const componentNames = this.getComponentName(position);
-    const searchPathActions = componentNames.map(this.searchFilePath);
-    const searchPromises = Promise.all(searchPathActions); // pass array of promises
-    
-    return searchPromises.then((paths) => {
-      filePaths = [].concat.apply([], paths);
-      
-      if (filePaths.length) {
-        let allPaths = [];
-        filePaths.forEach(filePath => {
-          allPaths.push(new vscode.Location(vscode.Uri.file(`${filePath.path}`),new vscode.Position(0,1) ))
-        });
-        return allPaths;
-      } else {
-        return undefined;
+    let filePaths = []
+    const componentNames = this.getComponentName(position)
+    const searchPathActions = componentNames.map(this.searchFilePath)
+    const searchPromises = Promise.all(searchPathActions) // pass array of promises
+
+    return searchPromises.then(
+      paths => {
+        filePaths = [].concat.apply([], paths)
+
+        if (filePaths.length) {
+          let allPaths = []
+          filePaths.forEach(filePath => {
+            allPaths.push(
+              new vscode.Location(vscode.Uri.file(`${filePath.path}`), new vscode.Position(0, 1))
+            )
+          })
+          return allPaths
+        } else {
+          return undefined
+        }
+      },
+      reason => {
+        return undefined
       }
-    }, (reason) => {
-      return undefined;
-    }); 
+    )
   }
 }
